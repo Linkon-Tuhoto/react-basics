7e251c37534e29eb6aef870006b2906cc90a5f05
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-08 22:33:53 +0300

    first commit

---
+++ src/components/header.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";  
+
+function Header(){
+    const [open, Setopen] = useState(false);
+    const [theme, Settheme] = useState("light");
+    return(
+        <div className="theme">
+            {/*Navbar*/}
+        <div className="flex justify-between items-center text-amber-800 dark:bg-amber-800 dark:text-white p-5">
+            <div>
+                <h1>Logo</h1>
+            </div>
+
+            {/* Desktop Nav */}
+        <div className="hidden sm:flex gap-5">
+            <span>Home</span>
+            <span>About Us</span>
+            <span>Services</span>
+            <span>Contact Us</span>
+        </div>
+         <button className="cursor-pointer" onClick={() => Settheme(theme === "light" ? "dark" : "light")}>
+            {theme === "light" ? "☀️" : "🌙"}
+            </button>
+        <button className="sm:hidden text-8xl cursor-pointer" onClick={() => Setopen(!open)}>=</button>
+        </div>
+        {/*Mobile Nav*/}
+        {open && (
+        <div className="md:hidden grid grid-cols-1 bg-blue-500 z-50 text-right p-5">
+            <span>Home</span>
+            <span>About Us</span>
+            <span>Services</span>
+            <span>Contact Us</span>
+        </div>)}
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 p-5 text-center font-semibold text-2xl sm:text-lg dark:bg-pink-600 dark:text-white">
+            <div className="rounded-3xl bg-amber-200 p-4 hover:bg-amber-400 hover:scale-105 transition-all" >Feature 1</div>
+            <div className="rounded-3xl bg-amber-200 p-4 hover:bg-amber-400 hover:scale-105 transition-all">Feature 2</div>
+            <div className="rounded-3xl bg-amber-200 p-4 hover:bg-amber-400 hover:scale-105 transition-all">Feature 3</div>
+            <div className="rounded-3xl bg-amber-200 p-4 hover:bg-amber-400 hover:scale-105 transition-all">Feature 4</div>
+            <div className="rounded-3xl bg-amber-200 p-4 hover:bg-amber-400 hover:scale-105 transition-all">Feature 5</div>
+            <div className="rounded-3xl bg-amber-200 p-4 hover:bg-amber-400 hover:scale-105 transition-all">Feature 6</div>
+        </div>
+        </div>
+    );
+}
+
+export default Header
