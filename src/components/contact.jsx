4bf5bf8e89867a5ef4031f50efc560cc2eddfe1c
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-27 14:42:14 +0300

    learnt event handling

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,10 +6,19 @@ import { useState } from 'react';
 function Contact() {
     const[name, setName] = useState("");
     const[message, setMessage] = useState("");
+    const handleSubmit = (e) => {
+        e.preventDefault();//stops page from reloading, thus avoid losing data
+        console.log("Name:", name);
+        console.log("Message:", message);
+
+        //after submitting, everything is cleared
+        setName("");
+        setMessage("");
+    };
   return (
     <div>
         <h2>Contact Form</h2>
-        <form className='bg-amber-300 mb-7 p-10'>
+        <form onSubmit={handleSubmit} className='bg-amber-300 mb-7 p-10'>
             <input type="text" placeholder='Your Name' value={name} onChange={(e) => setName(e.target.value)} className='bg-blue-400' />
             <textarea placeholder='Your Message' value={message} onChange={(e) => setMessage(e.target.value)} className='bg-blue-400'></textarea>
             <button>Submit</button>
