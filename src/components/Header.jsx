75b545e82b6cb986c88f88c4810edb58b07e9661
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-27 15:33:43 +0300

    popup message appearing after clicking button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ function Header(){
     const [open, Setopen] = useState(false);
     const [theme, Settheme] = useState("light");
     const [show, Setshow] = useState(false);
+    const [popup, setPopup] = useState(false);
     return(
         <div className="theme">
             {/*Navbar*/}
@@ -45,8 +46,22 @@ function Header(){
         <div className="relative before:absolute before:top-0 before:bottom-0 before:left-1/16 md:before:left-1/2 before:w-1 before:bg-gray-400 pl-20 pr-20">
             <div className="flex justify-start md:justify-start mb-8">
                 <div className="bg-gray-400 block absolute top-1/12 right-[53.75%] md:right-1/2 w-1 h-1 pl-[40%] "></div>
-                <div className="rounded-3xl bg-amber-200 p-40 hover:bg-amber-400 hover:scale-105 transition-all z-0" >Feature 1</div>
+                <div className="rounded-3xl bg-amber-200 p-35 hover:bg-amber-400 hover:scale-105 transition-all z-0" >
+                    <h1>Feature 1</h1>
+                    <button onClick={() => setPopup(!popup)} className="bg-green-500 text-white px-5 py-2 rounded">Read More</button>
+                    </div>
                 <div className="bg-blue-500 circle absolute top-1/12 right-1/2 w-4 h-4 rounded-full transform translate-x-1/2"></div>
+                {popup && (
+                    <div className="fixed h-8/12 w-11/12 inset-0 bg-white/40 backdrop-blur-md p-30 z-1000 space-y-50 text-center mt-0">
+                        <div className="ml-[130%] sm:ml-[100%]">
+                        <button onClick={() => setPopup(!popup)} className="cursor-pointer">X</button>
+                        </div>
+                        <div>
+                        <h2>Web development</h2>
+                        <p>I build responsive websites using React and Tailwind</p>
+                        </div>
+                    </div>
+                )}
             </div>
             <div className="flex justify-start md:justify-end mb-8">
                 <div className="bg-gray-400 block absolute top-1/4 right-[53.75%] md:left-1/2 w-1 h-1 pr-[40%]"></div>
