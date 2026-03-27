011fe431e46246e4e13037567048b2932ab44b2a
Linkon-Tuhoto <linkonngere@gmail.com>
2026-03-27 14:27:51 +0300

    learnt handle input changes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,14 +1,17 @@
 {/*Learning  event Handling- Event handling is how your app responds to user actions:clicking button, typing in input, submitting form*/}
 
 import React from 'react';
+import { useState } from 'react';
 
 function Contact() {
+    const[name, setName] = useState("");
+    const[message, setMessage] = useState("");
   return (
     <div>
         <h2>Contact Form</h2>
         <form className='bg-amber-300 mb-7 p-10'>
-            <input type="text" placeholder='Your Name' className='bg-blue-400' />
-            <textarea placeholder='Your Message' className='bg-blue-400'></textarea>
+            <input type="text" placeholder='Your Name' value={name} onChange={(e) => setName(e.target.value)} className='bg-blue-400' />
+            <textarea placeholder='Your Message' value={message} onChange={(e) => setMessage(e.target.value)} className='bg-blue-400'></textarea>
             <button>Submit</button>
         </form>
     </div>
